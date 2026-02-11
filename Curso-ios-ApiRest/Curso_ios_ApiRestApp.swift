a8303bf6a8b3cd0d0ce0c5a69c6b5c7052c6c54b
Fernando <dwk262@gmail.com>
2026-02-11 10:30:07 +0100

    Initial Commit

---
+++ Curso-ios-ApiRest/Curso_ios_ApiRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_ios_ApiRestApp.swift
+//  Curso-ios-ApiRest
+//
+//  Created by Equipo 9 on 11/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_ios_ApiRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
